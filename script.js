d679f4797be91e399898b3b83a35e90f09573a0d
USER STD 002 <gitautabby81@gmail.com>
2024-07-30 13:17:13 +0500

    my first commit

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-document.getElementById('contact-form').addEventListener('submit', function(event) {
+23document.getElementById('contact-form').addEventListener('submit', function(event) {
     event.preventDefault();
     alert('Thank you for contacting us!');
 });
